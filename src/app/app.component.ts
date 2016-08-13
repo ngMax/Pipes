c35eba30a70e48bff0b1bdb520112f15d9436d6d
sunil <sun.aec@gmail.com>
2016-08-13 16:34:31 +0530

    Final Upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
-
+import { DoublePipe } from './double.pipe';
+import { FilterPipe } from './filter.pipe';
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  pipes: [DoublePipe, FilterPipe]
 })
 export class AppComponent {
-  title = 'app works!';
+  myDate= new Date(2016, 7, 24);
+  values=['Milk', 'Bread', 'Bean'];
+  asyncValue=new Promise((resolve, reject)=>{
+    setTimeout(()=>resolve('Data is here') ,2000);
+  });
 }
